be3dcfd13c4d5d221cd198032e0b69d426d5e8d8
Omid Aiene <omid.aienad@gmail.com>
2022-02-28 16:59:59 +0330

    update #42 third-time refactoring login.compoenet 14001209

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SystemInformation } from 'src/app/shared/appModels';
 import { ApiEndPointService } from 'src/app/shared/services/api-end-point.service';
 import { ExtractSystemInfo } from 'src/app/shared/SharedClasses/extractSystemInfo';
+import { LoginPageUIText } from 'src/assets/Resources/projectInterfaceResources';
+import { getLogInPageUIText } from 'src/assets/Resources/projectResources';
 
 @Component({
   selector: 'map-login',
@@ -11,29 +13,14 @@ import { ExtractSystemInfo } from 'src/app/shared/SharedClasses/extractSystemInf
 })
 export class LoginComponent implements OnInit {
 
-  pageDirection:string;
-  Caption:string;
-  appVersion:string;
-  PoweredBy:string;
-  ReleaseDate:string;
-  logoImage:string;
-  wallpaper:string;
-
+  logInPageUIText: LoginPageUIText;
+  systemInfo: SystemInformation
   loginForm:FormGroup;
-
-  usernameLabel:string = "نام کاربری";
-  passwordLabel:string = "کلمه عبور";
   field_autocomplete:string = "off";
   field_autofocus:boolean = false;
-  logInBtnText:string = "ورود به حساب کاربری";
-  remmeberMeCheckBoxText:string = "مرا در این رایانه به خاطر نگه‌دار";
-  retrievePasswordLinkText:string = "بازیابی کلمه عبور";
-  hasAccountSpanText:string = "حساب کاربری ندارید؟";
-  createNewAccount:string = "ایجاد حساب کاربری جدید";
 
   hidePassword:boolean = true;
   remmeberMeIsChecked:boolean = true;
-
   rememberMeCheckBoxExistInLocalStorage:boolean = true;
 
   constructor(private apiEndPointService: ApiEndPointService,
@@ -44,24 +31,18 @@ export class LoginComponent implements OnInit {
     try {
 
       // 1. checking systemInformation
-      const systemInformation: SystemInformation = this.extractSystemInfo.getInfo();
+      const _systemInformation: SystemInformation = this.extractSystemInfo.getInfo();
 
-      if ( systemInformation ) {
+      if ( _systemInformation ) {
 
         // 2. get systemInformation from server
-        this.pageDirection = systemInformation.Direction;
-        this.Caption = systemInformation.Caption;
-        this.appVersion = systemInformation.Version;
-        this.PoweredBy = systemInformation.PoweredBy;
-        this.ReleaseDate = systemInformation.ReleaseDate;
-        this.logoImage = systemInformation.Logo;
-        this.wallpaper = systemInformation.Wallpaper;
+        this.systemInfo = _systemInformation;
+
+        // 3. set LogInPageUIText
+        this.logInPageUIText = getLogInPageUIText(this.systemInfo.Culture);
   
-        // 3. create LoginForm
-        this.loginForm = new FormGroup({
-          username: new FormControl('', [Validators.required]),
-          password: new FormControl('', [Validators.required]),
-        });
+        // 4. generate LoginForm
+        this.generateLoginForm();
 
       }
       
@@ -70,6 +51,15 @@ export class LoginComponent implements OnInit {
       console.log("Redirect to Page-404 ....");
       
     }
+
+  }
+
+  generateLoginForm() {
+
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
 
   }
 
