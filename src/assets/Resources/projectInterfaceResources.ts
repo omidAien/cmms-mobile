5eb2a1e7e3fb519abdedcfeee280753931fb8377
Omid Aiene <omid.aienad@gmail.com>
2022-02-28 16:30:39 +0330

    update #40 add Resources for App 14001209

---
+++ src/assets/Resources/projectInterfaceResources.ts
@@ -0,0 +1,9 @@
+export interface LoginPageUIText {
+    usernameLabelText:string;
+    passwordLabelText:string;
+    logInBtnText:string;
+    remmeberMeCheckBoxText:string;
+    retrievePasswordText:string;
+    hasAccountText:string;
+    createNewAccountText:string;
+}
