a6ce95f324b22f84ba6f8dc25324668c475b151d
Omid Aiene <omid.aienad@gmail.com>
2022-02-28 15:08:44 +0330

    update #34 initial-config for login-component 14001209

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SystemInformation } from 'src/app/shared/appModels';
+import { ApiEndPointService } from 'src/app/shared/services/api-end-point.service';
+import { ExtractSystemInfo } from 'src/app/shared/SharedClasses/extractSystemInfo';
 
 @Component({
   selector: 'map-login',
@@ -7,9 +11,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  pageDirection:string;
+  Caption:string;
+  appVersion:string;
+  PoweredBy:string;
+  ReleaseDate:string;
+  logoImage:string;
+  wallpaper:string;
 
-  ngOnInit(): void {
+  loginForm:FormGroup;
+
+  usernameLabel:string = "نام کاربری";
+  passwordLabel:string = "کلمه عبور";
+  field_autocomplete:string = "off";
+  field_autofocus:boolean = false;
+  logInBtnText:string = "ورود به حساب کاربری";
+  remmeberMeCheckBoxText:string = "مرا در این رایانه به خاطر نگه‌دار";
+  retrievePasswordLinkText:string = "بازیابی کلمه عبور";
+  hasAccountSpanText:string = "حساب کاربری ندارید؟";
+  createNewAccount:string = "ایجاد حساب کاربری جدید";
+
+  hidePassword:boolean = true;
+  remmeberMeIsChecked:boolean = false;
+
+  rememberMeCheckBoxExistInLocalStorage:boolean = false;
+
+  constructor(private apiEndPointService: ApiEndPointService,
+              private extractSystemInfo: ExtractSystemInfo) {}
+
+  ngOnInit() {
+
+    try {
+
+      // 1. checking systemInformation
+      const systemInformation: SystemInformation = this.extractSystemInfo.getInfo();
+
+      if ( systemInformation ) {
+
+        // 2. get systemInformation from server
+        this.pageDirection = systemInformation.Direction;
+        this.Caption = systemInformation.Caption;
+        this.appVersion = systemInformation.Version;
+        this.PoweredBy = systemInformation.PoweredBy;
+        this.ReleaseDate = systemInformation.ReleaseDate;
+        this.logoImage = systemInformation.Logo;
+        this.wallpaper = systemInformation.Wallpaper;
+  
+        // 3. create LoginForm
+        this.loginForm = new FormGroup({
+          username: new FormControl('', [Validators.required]),
+          password: new FormControl('', [Validators.required]),
+        });
+
+      }
+      
+    } catch (error) {
+
+      console.log("Redirect to Page-404 ....");
+      
+    }
+
+  }
+
+  onSubmitForm() {
+
+  }
+
+  onRetrievePassword() {
+
+  }
+
+  onCreateNewAccount() {
+
   }
 
 }
