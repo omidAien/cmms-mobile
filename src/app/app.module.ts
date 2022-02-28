2934081d6eb1a65f3671670062467d0cb449d8e8
Omid Aiene <omid.aienad@gmail.com>
2022-02-28 14:01:15 +0330

    update #23 add SharedModule in app.module.ts 14001209

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthGuard } from './shared/AuthLogic/auth.guard';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [
