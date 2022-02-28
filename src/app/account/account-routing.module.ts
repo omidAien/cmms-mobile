210a9521a5af631589b0d69ede04e6e846db4d6a
Omid Aiene <omid.aienad@gmail.com>
2022-02-28 11:35:57 +0330

    update #14 defining accountModule's routes in AccountRoutingModule 14001209

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { LoginComponent } from './login/login.component';
+import { SelectWorkGroupComponent } from './select-work-group/select-work-group.component';
+import { SelectWorkstationComponent } from './select-workstation/select-workstation.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignupComponent
+  },
+  {
+    path: "forget-password",
+    component: ForgetPasswordComponent
+  },
+  {
+    path: "select-work-group",
+    component: SelectWorkGroupComponent
+  },
+  {
+    path: "select-workstation",
+    component: SelectWorkstationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
