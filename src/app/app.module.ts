065da6dab23a27d531ebbc4bdcd8b5df8c32553a
Omid Aiene <omid.aienad@gmail.com>
2022-02-28 12:02:45 +0330

    update #18 add HttpClientModule to app.module.ts 14001209

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthGuard } from './shared/AuthLogic/auth.guard';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AuthGuard } from './shared/AuthLogic/auth.guard';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
