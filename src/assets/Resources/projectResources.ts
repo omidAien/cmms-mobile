b4ef887938d7e3fa494a8db43d7b8745897d25c9
Omid Aiene <omid.aienad@gmail.com>
2022-02-28 16:59:12 +0330

    update #41 clean projectResources from checking Culture and refactoring LogInPageUIText 14001209

--- src/assets/Resources/projectResources.ts
+++ src/assets/Resources/projectResources.ts
@@ -1,22 +1,21 @@
-import { SystemInformation } from "src/app/shared/appModels";
 import { LoginPageUIText } from "./projectInterfaceResources";
 
-let culture:string = "en";
-
-const systemInformation = sessionStorage.getItem("systemInformation");
-if ( systemInformation ) {
-    const data:SystemInformation = JSON.parse(systemInformation);
-    culture = data.Culture;
-};
 
 // LogIn Page 
 
-export const loginPageUIText: LoginPageUIText = {
-    usernameLabelText: culture === "fa" ? "نام کاربری" : "Username",
-    passwordLabelText: culture === "fa" ? "گذرواژه" : "Password",
-    logInBtnText: "fa" ? "ورود به حساب کاربری" : "LogIn",
-    remmeberMeCheckBoxText: culture === "fa" ? "مرا در این رایانه به خاطر نگه‌دار" : "Remmber Me",
-    retrievePasswordText: culture === "fa" ? "بازیابی کلمه عبور" : "Fotget Password",
-    hasAccountText: culture === "fa" ? "حساب کاربری ندارید؟" : "Do You Have Not An Account?",
-    createNewAccountText: culture === "fa" ? "ایجاد حساب کاربری جدید" : "Create New Account",
+export const getLogInPageUIText = (culture:string) => {
+
+    const loginPageUIText: LoginPageUIText = {
+        usernameLabelText: culture === "fa" ? "نام کاربری" : "Username",
+        passwordLabelText: culture === "fa" ? "گذرواژه" : "Password",
+        logInBtnText: "fa" ? "ورود به حساب کاربری" : "LogIn",
+        remmeberMeCheckBoxText: culture === "fa" ? "مرا در این رایانه به خاطر نگه‌دار" : "Remmber Me",
+        retrievePasswordText: culture === "fa" ? "بازیابی کلمه عبور" : "Fotget Password",
+        hasAccountText: culture === "fa" ? "حساب کاربری ندارید؟" : "Do You Have Not An Account?",
+        createNewAccountText: culture === "fa" ? "ایجاد حساب کاربری جدید" : "Create New Account",
+    }
+
+    return loginPageUIText;
+
 }
+
