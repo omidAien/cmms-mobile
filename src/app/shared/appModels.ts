c6ac7e56c3b1797b3131c0ac11765828056f2371
Omid Aiene <omid.aienad@gmail.com>
2022-02-28 14:01:57 +0330

    update #24 update appModels 14001209

--- src/app/shared/appModels.ts
+++ src/app/shared/appModels.ts
@@ -1,3 +1,9 @@
+export interface AppSettings {
+    API_URL_INTERNAL:string; 
+    API_URL_EXTERNAL:string; 
+    Origin_EXTERNAL:string
+};
+
 export interface ErrorModel {
     hasError: boolean,
     Code: number;
