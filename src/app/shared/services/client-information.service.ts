f9f098a0487e62b791b0b192a99a6957f81e2625
Omid Aiene <omid.aienad@gmail.com>
2022-03-05 11:20:02 +0330

    update #11 implementing ClientInformationService 14001214

--- src/app/shared/services/client-information.service.ts
+++ src/app/shared/services/client-information.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { ClientInformation } from '../appModels';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ClientInformationService {
 
-  constructor() { }
+  constructor(private deviceService: DeviceDetectorService, 
+              private cookieService: CookieService) { }
+
+
+  getDeviceInfo() {
+
+    const clientDeviceInformation:ClientInformation = {
+      browser: this.deviceService.browser,
+      browserVersion : this.deviceService.browser_version,
+      device : this.deviceService.device,
+      deviceType : this.deviceService.deviceType,
+      orientation : this.deviceService.orientation,
+      os : this.deviceService.os,
+      osVersion : this.deviceService.os_version,
+      ip : "",
+      mac: "",
+      agent : this.deviceService.userAgent,
+      longitude: 0,
+      latitude: 0
+    };
+
+    this.cookieService.set("clientInformation", JSON.stringify(clientDeviceInformation));
+
+  }
+
 }
