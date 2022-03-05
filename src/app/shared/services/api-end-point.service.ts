dc845c2666750baedc9a63c353f5b842b574f824
Omid Aiene <omid.aienad@gmail.com>
2022-03-05 11:11:32 +0330

    update #7 adding apiEndPoint related to Login 14001214

--- src/app/shared/services/api-end-point.service.ts
+++ src/app/shared/services/api-end-point.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthenticateParameters, AuthenticateResponse } from '../appModels';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +19,23 @@ export class ApiEndPointService {
 
   readExternalJsonFileFromAssets(filename:string) {
     return this.httpClient.get(filename, {responseType: 'text'});
+  }
+
+  private setHeaders(token:Required<string>) {
+    const headres = new HttpHeaders({
+      "Authorization" : token,
+      "Content-Type" : "application/json; charset=utf-8",
+    });
+    return headres
+  }
+
+  loginProcedure(authenticateParameters:AuthenticateParameters): Observable<AuthenticateResponse> {
+
+    const headers = { 'content-type': 'application/json'}; 
+    const requestURL:Required<string> = this.baseURL.concat("Login/athenticate");
+    const body:Required<string> = JSON.stringify(authenticateParameters);
+
+    return this.httpClient.post<AuthenticateResponse>(requestURL, body, { headers: headers });
   }
 
 }
