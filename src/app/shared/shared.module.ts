66180fbff7111a0c5c975fa075b8dd83f5f22ed6
Omid Aiene <omid.aienad@gmail.com>
2022-03-05 11:20:49 +0330

    update #12 add ClientInformationService to sharedModule 14001214

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { GetInitialDataService } from './services/get-initial-data.service';
+import { ClientInformationService } from './services/client-information.service';
 
 
 
@@ -13,6 +14,7 @@ import { GetInitialDataService } from './services/get-initial-data.service';
   ],
   providers: [
     GetInitialDataService,
+    ClientInformationService
   ]
 })
 export class SharedModule { }
