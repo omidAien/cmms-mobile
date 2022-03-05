ae0a47d7ab8f2a6b4ecb6096a74c1852cbf0ae15
Omid Aiene <omid.aienad@gmail.com>
2022-03-05 11:10:54 +0330

    update #6 adding some interfaces related to Athentication to appModels 14001214

--- src/app/shared/appModels.ts
+++ src/app/shared/appModels.ts
@@ -29,3 +29,52 @@ export interface SystemInformationCore {
     Error: ErrorModel | null;
     dtSystemInformation: SystemInformation[];
 }
+
+export interface clientInformation {
+    browser: string;
+    browserVersion: string;
+    device: string;
+    deviceType: string;
+    orientation: string;
+    os: string;
+    osVersion: string;
+    ip: string;
+    mac: string;
+    agent: string;
+    latitude: number;
+    longitude: number;
+}
+
+export interface Workstation {
+    PK_ObjectID:number;
+    Caption: string;
+}
+
+export interface UserWorkgroup {
+    PK_WorkgroupID: number;
+    Workgroup: string;
+}
+
+export interface UserProject {
+    PK_ProjectID: number;
+    Project: string;
+    isDefault: boolean;
+}
+
+export interface AuthenticateParameters {
+    userID:string;
+    userPSW:string;
+    workstationID:number;
+    workgroupID:number;
+    clientInformation:clientInformation;
+}
+
+export interface AuthenticateResponse {
+    Error: ErrorModel;
+    Token: string | null;
+    UserName: string;
+    Workstations: Workstation[];
+    Workgroups: UserWorkgroup[] | null;
+    DefaultWorkgroupID: number | UserWorkgroup;
+    Projects: UserProject[];
+}
