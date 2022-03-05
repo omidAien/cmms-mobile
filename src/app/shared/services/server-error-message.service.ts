91241360ca2d0c8f159b98682e9a1c75d9c0c422
Omid Aiene <omid.aienad@gmail.com>
2022-03-05 11:49:22 +0330

    update #16 implementing ServerErrorMessageService 14001214

--- src/app/shared/services/server-error-message.service.ts
+++ src/app/shared/services/server-error-message.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerErrorMessageService {
 
+  private serverErrorMessageSubject = new BehaviorSubject<string>(null);
+  public serverErrorMessage$: Observable<string> = this.serverErrorMessageSubject.asObservable();
+
   constructor() { }
+
+  setMessage(msg: string) {
+    this.serverErrorMessageSubject.next(msg);
+  }
+  
 }
