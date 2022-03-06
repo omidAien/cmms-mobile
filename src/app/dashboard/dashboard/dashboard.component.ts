7f86ad3ab5fcfee5bc45321403bdd129d115efd2
Omid Aiene <omid.aienad@gmail.com>
2022-03-06 10:20:04 +0330

    update #45 adding userFullname & userPositin-work in DashboardCompoenet 14001215

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SystemInformation, UserWorkgroup } from 'src/app/shared/appModels';
+import { ExtractSystemInfo } from 'src/app/shared/SharedClasses/extractSystemInfo';
+import { HandleSessionstorage } from 'src/app/shared/SharedClasses/HandleSessionStorage';
 
 @Component({
   selector: 'map-dashboard',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  systemInfo: SystemInformation | null = null;
+  userFullname:string = "UnKnown";
+  userPosition:string = "UnKnown";
+
+  constructor(private handleSessionstorage: HandleSessionstorage,
+              private extractSytemInfo: ExtractSystemInfo) { }
 
   ngOnInit(): void {
+
+    const systemInfo$:Observable<SystemInformation> = this.extractSytemInfo.systemInfo$;
+    
+    systemInfo$.subscribe((_systemInfo: SystemInformation) => {
+
+      if ( _systemInfo ) {
+
+        this.systemInfo = _systemInfo;
+        this.userFullname = this.handleSessionstorage.get("userFullName");
+
+        const userDefaultWorkGroup: UserWorkgroup = this.handleSessionstorage.get("userDefaultWorkGroup");
+        this.userPosition = userDefaultWorkGroup.Workgroup;
+
+      }
+
+    });
+
   }
 
 }
