c4f3b14e3d9a6234caf6338fdb5334e06d2dff8c
Omid Aiene <omid.aienad@gmail.com>
2022-03-06 14:47:53 +0330

    update #52 adding iconClick-func in dashboardCompoenet 14001215

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { faArrowRightFromBracket, faGear, faHouse, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { Observable } from 'rxjs';
 import { SystemInformation, UserWorkgroup } from 'src/app/shared/appModels';
 import { ExtractSystemInfo } from 'src/app/shared/SharedClasses/extractSystemInfo';
@@ -9,11 +10,15 @@ import { HandleSessionstorage } from 'src/app/shared/SharedClasses/HandleSession
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
   systemInfo: SystemInformation | null = null;
   userFullname:string = "UnKnown";
   userPosition:string = "UnKnown";
+
+  faArrowRightFromBracketIcon: IconDefinition = faArrowRightFromBracket;
+  faGearIcon: IconDefinition = faGear;
+  faHouseIcon: IconDefinition = faHouse;
 
   constructor(private handleSessionstorage: HandleSessionstorage,
               private extractSytemInfo: ExtractSystemInfo) { }
@@ -36,6 +41,34 @@ export class DashboardComponent implements OnInit {
 
     });
 
+  }
+
+  iconClick(event:any, iconOperation:string): void {
+
+    const eventTarget = event.target;
+
+    // 1. rest 
+    const footerElement = eventTarget.closest("footer") as HTMLElement;
+    const allIcons = footerElement.querySelectorAll("fa-icon");
+    allIcons.forEach((faIcon) => faIcon.classList.remove("fa-icon-active"));
+
+    // 2. imply
+    const faIconElement: HTMLElement = eventTarget.closest("fa-icon") ? eventTarget.closest("fa-icon") : eventTarget.querySelector("fa-icon");
+    const iconOperationExtracted:string = faIconElement.dataset.iconOperation;
+
+    if ( iconOperationExtracted === iconOperation ) {
+
+      faIconElement.classList.add("fa-icon-active");
+
+    }
+  }
+
+  ngAfterViewInit(): void {
+
+    setTimeout(() => {
+      document.querySelector('[data-icon-operation="dashboard"]').closest("section").click();
+    }, 500);
+    
   }
 
 }
