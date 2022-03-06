d8bb9b63d4fd3d0f79cddc05375190abc421314a
Omid Aiene <omid.aienad@gmail.com>
2022-03-06 15:07:19 +0330

    update #53 using map-footer component inside dashboard.component 14001215

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { faArrowRightFromBracket, faGear, faHouse, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { Observable } from 'rxjs';
 import { SystemInformation, UserWorkgroup } from 'src/app/shared/appModels';
 import { ExtractSystemInfo } from 'src/app/shared/SharedClasses/extractSystemInfo';
@@ -15,10 +14,6 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   systemInfo: SystemInformation | null = null;
   userFullname:string = "UnKnown";
   userPosition:string = "UnKnown";
-
-  faArrowRightFromBracketIcon: IconDefinition = faArrowRightFromBracket;
-  faGearIcon: IconDefinition = faGear;
-  faHouseIcon: IconDefinition = faHouse;
 
   constructor(private handleSessionstorage: HandleSessionstorage,
               private extractSytemInfo: ExtractSystemInfo) { }
@@ -43,28 +38,9 @@ export class DashboardComponent implements OnInit, AfterViewInit {
 
   }
 
-  iconClick(event:any, iconOperation:string): void {
-
-    const eventTarget = event.target;
-
-    // 1. rest 
-    const footerElement = eventTarget.closest("footer") as HTMLElement;
-    const allIcons = footerElement.querySelectorAll("fa-icon");
-    allIcons.forEach((faIcon) => faIcon.classList.remove("fa-icon-active"));
-
-    // 2. imply
-    const faIconElement: HTMLElement = eventTarget.closest("fa-icon") ? eventTarget.closest("fa-icon") : eventTarget.querySelector("fa-icon");
-    const iconOperationExtracted:string = faIconElement.dataset.iconOperation;
-
-    if ( iconOperationExtracted === iconOperation ) {
-
-      faIconElement.classList.add("fa-icon-active");
-
-    }
-  }
-
   ngAfterViewInit(): void {
 
+    // clicking on the home-icon on footer programmatically
     setTimeout(() => {
       document.querySelector('[data-icon-operation="dashboard"]').closest("section").click();
     }, 500);
