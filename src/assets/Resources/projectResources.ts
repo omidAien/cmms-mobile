c1419bde171888180e2689556561ee1959ddce62
Omid Aiene <omid.aienad@gmail.com>
2022-04-13 16:44:48 +0430

    update #12 removing unnecceary items from projectResources.ts 14010124

--- src/assets/Resources/projectResources.ts
+++ src/assets/Resources/projectResources.ts
@@ -1,34 +1,4 @@
-import { LoginPageUIText, SelectWorkGroupUIText, SingleErrorMessage } from "./projectInterfaceResources";
-
-// LogIn Page 
-export const getLogInPageUIText = (culture:string): LoginPageUIText => {
-
-    const loginPageUIText: LoginPageUIText = {
-        usernameLabelText: culture === "fa" ? "نام کاربری" : "Username",
-        passwordLabelText: culture === "fa" ? "گذرواژه" : "Password",
-        logInBtnText: "fa" ? "ورود به حساب کاربری" : "LogIn",
-        remmeberMeCheckBoxText: culture === "fa" ? "مرا در این رایانه به خاطر نگه‌دار" : "Remmber Me",
-        retrievePasswordText: culture === "fa" ? "بازیابی کلمه عبور" : "Fotget Password",
-        hasAccountText: culture === "fa" ? "حساب کاربری ندارید؟" : "Do You Have Not An Account?",
-        createNewAccountText: culture === "fa" ? "ایجاد حساب کاربری جدید" : "Create New Account",
-    }
-
-    return loginPageUIText;
-
-}
-
-// Select-Work_Group Page
-export const getSelectWorkGroupUIText = (culture:string): SelectWorkGroupUIText => {
-
-    const selectWorkGroupUIText: SelectWorkGroupUIText = {
-        selectWorkGroupLabel: culture === "fa" ? "انتخاب گروه کاری" : "Select WorkGroup",
-        selectWorkGroupTitle: culture === "fa" ? "لطفا گروه کاری پیش فرض خود را انتخاب نمایید" : "Please Select Your Default Wrokgroup",
-        redirectToDashboard: culture === "fa" ? "ورود به صفحه کاری" : "Login"
-    };
-
-    return selectWorkGroupUIText;
-    
-}
+import { SingleErrorMessage } from "./projectInterfaceResources";
 
 // ServerErrorMessage
 export const ServerErrorMessageResources: SingleErrorMessage = {
