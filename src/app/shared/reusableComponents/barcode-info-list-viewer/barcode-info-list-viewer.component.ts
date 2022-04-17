0a46cb5cfef49afe24fc0108809b5dab60210558
Omid Aiene <omid.aienad@gmail.com>
2022-04-17 14:47:49 +0430

    update #15 initial implementing of BarcodeInfoListViewerComponent & BarcodeInfoListService 14010128

--- src/app/shared/reusableComponents/barcode-info-list-viewer/barcode-info-list-viewer.component.ts
+++ src/app/shared/reusableComponents/barcode-info-list-viewer/barcode-info-list-viewer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeInfoListService } from '../../services/barcode-info-list.service';
 
 @Component({
   selector: 'map-barcode-info-list-viewer',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarcodeInfoListViewerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public barcodeInfoListService: BarcodeInfoListService) { }
 
   ngOnInit(): void {
   }
--- src/app/shared/services/barcode-info-list.service.ts
+++ src/app/shared/services/barcode-info-list.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BarcodeInfoListService {
 
+  private barcodeInfoListSubject = new BehaviorSubject<any[]>(null);
+  public barcodeInfoList$: Observable<any[]> = this.barcodeInfoListSubject.asObservable();
+
   constructor() { }
+
+  set() {}
+
+  reset() {}
+
 }
