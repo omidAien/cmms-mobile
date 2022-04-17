67478dcb2af89dd24b1fd1b128cf84ae63e7b739
Omid Aiene <omid.aienad@gmail.com>
2022-04-17 13:47:46 +0430

    update #12 starting with SwitcherModule 14010128

---
+++ src/app/switcher/switcher.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SwitcherRoutingModule } from './switcher-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SwitcherRoutingModule
+  ]
+})
+export class SwitcherModule { }
