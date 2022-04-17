26ca17e4cb00b8ba367988ab9dc6bd99dc362e78
Omid Aiene <omid.aienad@gmail.com>
2022-04-17 09:34:42 +0430

    update #3 using of PWAItemsService in DashboardComponent 14010128

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,12 +1,9 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { EntryInputs, PWAItems, PWAItemsResponse, SystemInformation, UserWorkgroup } from 'src/app/shared/appModels';
-import { ApiEndPointService } from 'src/app/shared/services/api-end-point.service';
+import { BackButton, PWAItems, SystemInformation, UserWorkgroup } from 'src/app/shared/appModels';
 import { BackButtonService } from 'src/app/shared/services/back-button.service';
-import { LoadingService } from 'src/app/shared/services/loading.service';
-import { GeneralErrorMessage } from 'src/app/shared/SharedClasses/errorHandlingClass';
 import { HandleSessionstorage } from 'src/app/shared/SharedClasses/HandleSessionStorage';
+import { PWAItemsService } from '../Services/pwaitems.service';
 
 @Component({
   selector: 'map-dashboard',
@@ -19,15 +16,9 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   userFullname:string = "UnKnown";
   userPosition:string = "UnKnown";
 
-  private PWAItemsSubject = new BehaviorSubject<PWAItems[]>(null);
-  PWAItems$: Observable<PWAItems[]> = this.PWAItemsSubject.asObservable();
-
   constructor(private handleSessionstorage: HandleSessionstorage,
-              private apiEndPointService: ApiEndPointService,
-              private cookieService: CookieService,
-              private loadingService: LoadingService,
-              private backButtonService: BackButtonService,
-              private generalErrorMessage: GeneralErrorMessage) { }
+              public pwaItemsService: PWAItemsService,
+              private backButtonService: BackButtonService) { }
 
   ngOnInit(): void {
     
@@ -51,42 +42,12 @@ export class DashboardComponent implements OnInit, AfterViewInit {
 
   PWAItemsHandler() {
 
-    const token:string = "bearer ".concat(JSON.parse(this.cookieService.get("token")));
-    const userDefaultWorkGroup:UserWorkgroup = this.handleSessionstorage.get("userDefaultWorkGroup");
-    const workgroupID:number = userDefaultWorkGroup.PK_WorkgroupID;
+    /* this function will be called in two places. 
+    First, when user has been moved to dashboard-page first of all, second, when user refresh the page */
 
-    const getPWAItems$: Observable<PWAItemsResponse> = this.apiEndPointService.getPWAItems(token, this.entryInputsForPWAItems(workgroupID));
+    const lastBackBtn: BackButton = this.backButtonService.peek();
 
-    this.loadingService
-        .showPreLoaderUntilCompleted(getPWAItems$, true, 500)
-        .subscribe((response: PWAItemsResponse) => {
-
-          if ( !response.Error.hasError ) {
-  
-            this.PWAItemsSubject.next(response.PWA);
-
-          }
-
-          else {
-
-            this.generalErrorMessage.handleServerSideError(response.Error.Message, this.pageInfo.Direction);
-
-          }
-
-        });
-
-  }
-
-  entryInputsForPWAItems(workgroupID:number): EntryInputs {
-
-    const entryInputs: EntryInputs = {
-      objectID: 0,
-      workgroupID: workgroupID,
-      fields: [],
-      rowID: 0
-    };
-
-    return entryInputs;
+    this.pwaItemsService.getItems(lastBackBtn.ObjectID, this.pageInfo.Direction);
 
   }
 
@@ -125,6 +86,10 @@ export class DashboardComponent implements OnInit, AfterViewInit {
       RouterPath: extractedRouterPath,
       Active: true  
     });
+
+    // 6. updating PWAItems
+    this.pwaItemsService.reset();
+    this.pwaItemsService.getItems(extractedObjectID, this.pageInfo.Direction);
 
   }
 
