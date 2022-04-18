28a95d02ac442dcbea1cf13d741b2c21808d1438
Omid Aiene <omid.aienad@gmail.com>
2022-04-18 15:30:28 +0430

    update #4 creating TaskTypeCodeInfo to handle taskTypeCodes 14010129

--- src/app/shared/taskTypeManager/taskTypes.ts
+++ src/app/shared/taskTypeManager/taskTypes.ts
@@ -1,7 +1,41 @@
-export const taskTypeManager = {
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 
-    
+interface TaskTypeCode {
+    componentName: string;
+    routerPath: string;
+}
 
+class TaskTypeCodeInfo {
 
+    protected TaskTypeCodes: { [key:string]: TaskTypeCode } = {
+        "0": {
+            componentName: "DashboardComponent",
+            routerPath: "/dashboard"
+        },
+        "2": {
+            componentName: "WarehouseReceiptTypeComponent",
+            routerPath: "/swcomp/wrt"
+        }
+    }
 
-} 
+}
+
+@Injectable()
+export class TaskTypeCodeHandler extends TaskTypeCodeInfo {
+
+    constructor(private router: Router) {
+
+        super();
+
+    }
+
+    navigator(taskTypeCode: number) {
+
+        const routerPath: string = this.TaskTypeCodes[taskTypeCode].routerPath;
+
+        this.router.navigateByUrl(routerPath);
+ 
+    }
+
+}
