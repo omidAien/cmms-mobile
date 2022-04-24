8e7d9b0a7703463330a91098cd497f9fa5eaf010
Omid Aiene <omid.aienad@gmail.com>
2022-04-24 11:10:14 +0430

    update #4 adding form DocumentInfoViewerComponent 14010204

--- src/app/shared/reusableComponents/document-info-viewer/document-info-viewer.component.ts
+++ src/app/shared/reusableComponents/document-info-viewer/document-info-viewer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ResourceMainStore } from '../../ResourceManager/resourseMainStore';
 
 @Component({
@@ -8,7 +9,11 @@ import { ResourceMainStore } from '../../ResourceManager/resourseMainStore';
 })
 export class DocumentInfoViewerComponent implements OnInit {
 
-  documentTitleInfo: { number: string; date: string };
+  @Output() formOutputHandler = new EventEmitter<FormGroup>();
+
+  documentTitleInfo: { number: string; date: string, fieldLabel: string };
+  formAppearance: string = "outline";
+  documentForm: FormGroup;
 
   constructor(private resourceMainStore: ResourceMainStore) { }
 
@@ -16,14 +21,27 @@ export class DocumentInfoViewerComponent implements OnInit {
 
     this.setDocumentInfo();
 
+    this.formGenerator();
+
   }
 
   setDocumentInfo() {
 
     this.documentTitleInfo = {
       number: this.resourceMainStore.getDocumentNumberTextResource(),
-      date: this.resourceMainStore.getDocumentDateTextResource()
+      date: this.resourceMainStore.getDocumentDateTextResource(),
+      fieldLabel: this.resourceMainStore.getDocumentDescriptionFieldLabelTextResource()
     };
+
+  }
+
+  formGenerator() {
+
+    this.documentForm = new FormGroup({
+      description: new FormControl('')
+    });
+
+    this.formOutputHandler.emit(this.documentForm);
 
   }
 
