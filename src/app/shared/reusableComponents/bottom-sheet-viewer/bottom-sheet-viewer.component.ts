bc50b9e928977cddf95f65e6b2e6a3ed4d50c48a
Omid Aiene <omid.aienad@gmail.com>
2022-04-24 15:42:35 +0430

    update #8 generating BotoomSheetViewerComponent 14010204

---
+++ src/app/shared/reusableComponents/bottom-sheet-viewer/bottom-sheet-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'map-bottom-sheet-viewer',
+  templateUrl: './bottom-sheet-viewer.component.html',
+  styleUrls: ['./bottom-sheet-viewer.component.scss']
+})
+export class BottomSheetViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
