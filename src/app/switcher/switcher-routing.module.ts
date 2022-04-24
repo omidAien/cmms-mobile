6f01bf4218f3d46eb864c75daa35ae7d2a880ff4
Omid Aiene <omid.aienad@gmail.com>
2022-04-24 12:14:53 +0430

    update #7 adding new routers to switcher-routing.module.ts 14010204

--- src/app/switcher/switcher-routing.module.ts
+++ src/app/switcher/switcher-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailFormComponent } from './warehouse-receipt-type/detail-form/detail-form.component';
+import { DetailViewerComponent } from './warehouse-receipt-type/detail-viewer/detail-viewer.component';
 import { WarehouseReceiptTypeComponent } from './warehouse-receipt-type/warehouse-receipt-type.component';
 
 const routes: Routes = [
   {
     path: "wrt",
-    component: WarehouseReceiptTypeComponent
+    component: WarehouseReceiptTypeComponent,
+  },
+  {
+    path: "wrt/detail-form",
+    component: DetailFormComponent
+  },
+  {
+    path: "wrt/detail-viewer",
+    component: DetailViewerComponent
   }
 ];
 
