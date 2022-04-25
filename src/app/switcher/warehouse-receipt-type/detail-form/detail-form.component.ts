f5a9c916d4f625661ef71054917051152404bc75
Omid Aiene <omid.aienad@gmail.com>
2022-04-25 16:19:06 +0430

    update #3 initial-implementing of DetailFormComponent 14010205

--- src/app/switcher/warehouse-receipt-type/detail-form/detail-form.component.ts
+++ src/app/switcher/warehouse-receipt-type/detail-form/detail-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormBuilderEventEmitterHandler, SystemInformation } from 'src/app/shared/appModels';
+import { ResourceMainStore } from 'src/app/shared/ResourceManager/resourseMainStore';
+import { HandleSessionstorage } from 'src/app/shared/SharedClasses/HandleSessionStorage';
 
 @Component({
   selector: 'map-detail-form',
@@ -7,9 +11,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailFormComponent implements OnInit {
 
-  constructor() { }
+  pageInfo: Pick<SystemInformation, "Direction" | "Culture">;
+  submitChangesButtonText: string;
+  formBuilderHandler: FormGroup;
+
+  constructor(private handleSessionstorage: HandleSessionstorage,
+              private resourceMainStore: ResourceMainStore) { }
 
   ngOnInit(): void {
+
+    this.detectPageInfo();
+
+    this.setCultureForResourceMainStore();
+
+    this.setSubmitChangesButtonTextResource();
+
+  }
+
+  detectPageInfo() {
+
+    this.pageInfo = this.handleSessionstorage.get("pageInfo");
+
+  }
+
+  setCultureForResourceMainStore() {
+
+    this.resourceMainStore.culture = this.pageInfo.Culture;
+
+  }
+
+  setSubmitChangesButtonTextResource() {
+
+    this.submitChangesButtonText = this.resourceMainStore.getChangeApplyButtonTextResource();
+
+  }
+
+  formBuilderOutputHandler(event: FormBuilderEventEmitterHandler) {
+
+    this.formBuilderHandler = event.formGroup;
+
+  }
+
+  submitChages(event: any) {
+
+        // if ( this.formBuilderEventHandler.barcodeFormControl.value.length < 12 ) {
+
+    //   const errorMsg: string = this.resourceMainStore.getInvalidBarcodeLengthErrorMessageTextResource();
+    //   this.generalErrorMessage.handleClientSideError(errorMsg, this.pageInfo.Direction);
+
+    // } else if ( this.formBuilderEventHandler.formGroup.invalid ) {
+
+    //   this.formBuilderEventHandler.formGroup.markAllAsTouched();
+
+    // } else {
+      
+    //   console.log(this.formBuilderEventHandler.formGroup.value);
+    
+    // }
+
+    if ( this.formBuilderHandler.invalid ) {
+
+      this.formBuilderHandler.markAllAsTouched();
+
+    } else {
+      
+      console.log(this.formBuilderHandler.value);
+
+      this.formBuilderHandler.reset();
+    
+    }
+
   }
 
 }
