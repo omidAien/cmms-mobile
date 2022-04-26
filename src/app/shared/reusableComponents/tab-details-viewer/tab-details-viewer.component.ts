9419e26462eebbcd1e8661f93392d5f9f026901a
Omid Aiene <omid.aienad@gmail.com>
2022-04-26 15:49:08 +0430

    update #6 adding error-handling inside html-file for TabDetailsViewerComponent 14010206

--- src/app/shared/reusableComponents/tab-details-viewer/tab-details-viewer.component.ts
+++ src/app/shared/reusableComponents/tab-details-viewer/tab-details-viewer.component.ts
@@ -1,9 +1,10 @@
-import { Component, DoCheck, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { MatTabGroup } from '@angular/material/tabs';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { SystemInformation } from '../../appModels';
+import { ResourceMainStore } from '../../ResourceManager/resourseMainStore';
 import { LoadingService } from '../../services/loading.service';
 import { HandleSessionstorage } from '../../SharedClasses/HandleSessionStorage';
+import { faTriangleExclamation, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'map-tab-details-viewer',
@@ -13,39 +14,92 @@ import { HandleSessionstorage } from '../../SharedClasses/HandleSessionStorage';
 })
 export class TabDetailsViewerComponent implements OnInit {
 
-  sampleTabData: { Label:string; Content: string }[] = [
-    { Label: "آمار سریالی", Content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat pretium nibh ipsum consequat nisl. Tempor id eu nisl nunc mi ipsum faucibus vitae. Vestibulum lectus mauris ultrices eros in cursus turpis massa. Tempor nec feugiat nisl pretium fusce id velit ut. Porta lorem mollis aliquam ut porttitor leo a diam. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Ut tortor pretium viverra suspendisse potenti nullam ac. Donec enim diam vulputate ut pharetra. Gravida quis blandit turpis cursus. Imperdiet proin fermentum leo vel orci porta non. Ac feugiat sed lectus vestibulum mattis. Amet nulla facilisi morbi tempus iaculis. Nulla pharetra diam sit amet nisl. Auctor neque vitae tempus quam pellentesque.
+  sampleTabData: { Label:string; Content: { Barcode: string; Detail: { Caption: string; Value: string }[] }[] }[] = [
 
-    Id aliquet lectus proin nibh nisl condimentum id venenatis. Cras semper auctor neque vitae tempus quam. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Ut tellus elementum sagittis vitae et. Blandit massa enim nec dui nunc mattis. Arcu vitae elementum curabitur vitae nunc. Morbi enim nunc faucibus a pellentesque sit amet. Auctor urna nunc id cursus. Ut venenatis tellus in metus vulputate. Blandit massa enim nec dui.
-    
-    At urna condimentum mattis pellentesque id nibh tortor id. Turpis egestas integer eget aliquet nibh praesent. At tellus at urna condimentum mattis pellentesque. Nisi lacus sed viverra tellus in hac habitasse platea dictumst. Eu non diam phasellus vestibulum lorem sed risus. Tincidunt praesent semper feugiat nibh sed pulvinar proin gravida. Est ullamcorper eget nulla facilisi. Euismod lacinia at quis risus sed vulputate odio. Ipsum consequat nisl vel pretium lectus quam id. Consectetur purus ut faucibus pulvinar. Semper auctor neque vitae tempus quam pellentesque nec. Semper viverra nam libero justo laoreet sit amet cursus sit. Nunc mattis enim ut tellus elementum sagittis vitae et.
-    
-    In ante metus dictum at tempor commodo ullamcorper a. Felis imperdiet proin fermentum leo. Fermentum leo vel orci porta non pulvinar neque. Vel pharetra vel turpis nunc. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Tellus mauris a diam maecenas sed enim ut. Nullam vehicula ipsum a arcu cursus vitae congue. Adipiscing elit duis tristique sollicitudin. Tristique risus nec feugiat in fermentum posuere urna nec tincidunt. Et malesuada fames ac turpis. Eget arcu dictum varius duis at consectetur lorem donec massa. Nibh tellus molestie nunc non. Cursus in hac habitasse platea. Tellus in metus vulputate eu scelerisque felis. Amet commodo nulla facilisi nullam vehicula. Suspendisse interdum consectetur libero id faucibus. Sit amet commodo nulla facilisi.
-    
-    Lectus proin nibh nisl condimentum id. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Turpis cursus in hac habitasse platea dictumst quisque sagittis purus. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Tincidunt praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Pellentesque adipiscing commodo elit at. Tincidunt id aliquet risus feugiat in ante. Placerat orci nulla pellentesque dignissim enim. Vel pretium lectus quam id leo in vitae turpis. Ac turpis egestas integer eget aliquet nibh. Et odio pellentesque diam volutpat commodo sed egestas egestas. Penatibus et magnis dis parturient montes nascetur ridiculus mus. Id neque aliquam vestibulum morbi blandit. Lobortis scelerisque fermentum dui faucibus in ornare. Eu consequat ac felis donec et odio pellentesque diam volutpat.` },
-    { Label: "آمار تجمعی", Content: "Content Two" },
-    { Label: "خطا", Content: "Content Three" },
+    { Label: "آمار سریالی", Content: [
+      { Barcode: "000000000000", Detail: [
+        {
+          Caption: "رنگ",
+          Value: "قرمز"
+        },
+        {
+          Caption: "ضخامت",
+          Value: "56 میلی متر"
+        }] 
+      },
+      { Barcode: "000001100000", Detail: [
+        {
+          Caption: "رنگ",
+          Value: "آبی"
+        },
+        {
+          Caption: "ضخامت",
+          Value: "56 میلی متر"
+        }] 
+      }] 
+    },
+    { Label: "آمار تجمعی", Content: [
+      { Barcode: "000000000000", Detail: [
+        {
+          Caption: "رنگ",
+          Value: "قرمز"
+        },
+        {
+          Caption: "ضخامت",
+          Value: "56 میلی متر"
+        },
+        {
+          Caption: "تعداد",
+          Value: "36"
+        }] 
+      },
+      { Barcode: "000001100000", Detail: [
+        {
+          Caption: "رنگ",
+          Value: "آبی"
+        },
+        {
+          Caption: "ضخامت",
+          Value: "56 میلی متر"
+        },
+        {
+          Caption: "تعداد",
+          Value: "36"
+        }] 
+      }] 
+    }
   ];
 
-  tabData$: Observable<{ Label:string; Content: string }[]>;
+  faTriangleExclamationIcon: IconDefinition = faTriangleExclamation;
+  tabData$: Observable<{ Label:string; Content: { Barcode: string; Detail: { Caption: string; Value: string }[] }[] }[]> = of(this.sampleTabData);
   pageInfo: Pick<SystemInformation, "Direction" | "Culture">;
-
-  @ViewChild("matTabGroup") matTabGroup: MatTabGroup;
+  noDataMessage: string;
 
   constructor(private loadingService: LoadingService,
+              private resourceMainStore: ResourceMainStore,
               private handleSessionstorage: HandleSessionstorage) { }
 
   ngOnInit(): void {
 
     this.getPageInfo();
     
-    this.tabData$ = this.loadingService.showPreLoaderUntilCompleted(of(this.sampleTabData), true, 1000);
+    this.setNoDataMessageTextHandler();
+
+    this.tabData$ = this.loadingService.showPreLoaderUntilCompleted(this.tabData$, true, 500);
 
   }
 
   getPageInfo() {
 
     this.pageInfo = this.handleSessionstorage.get("pageInfo");
+
+  }
+
+  setNoDataMessageTextHandler() {
+
+    this.resourceMainStore.culture = this.pageInfo.Culture;
+
+    this.noDataMessage = this.resourceMainStore.getNoDataForRepresentationTextResource();
 
   }
 
