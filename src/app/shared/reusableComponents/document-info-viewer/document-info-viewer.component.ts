0171ec4af9ecfa8960345379c99725e58abbfa84
Omid Aiene <omid.aienad@gmail.com>
2022-04-27 16:38:29 +0430

    update #12 using FormBuilderComponent in DocumentInfoViewerComponent 14010207

--- src/app/shared/reusableComponents/document-info-viewer/document-info-viewer.component.ts
+++ src/app/shared/reusableComponents/document-info-viewer/document-info-viewer.component.ts
@@ -1,6 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { ResourceMainStore } from '../../ResourceManager/resourseMainStore';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'map-document-info-viewer',
@@ -9,41 +8,11 @@ import { ResourceMainStore } from '../../ResourceManager/resourseMainStore';
 })
 export class DocumentInfoViewerComponent implements OnInit {
 
-  @Output() formOutputHandler = new EventEmitter<FormGroup>();
-
-  documentTitleInfo: { number: string; date: string, fieldLabel: string };
-  documentForm: FormGroup;
-
-  constructor(private resourceMainStore: ResourceMainStore) { }
+  constructor() { }
 
   ngOnInit(): void {
 
-    this.setDocumentInfo();
-
-    this.formGenerator();
-
   }
 
-  setDocumentInfo() {
-
-    this.documentTitleInfo = {
-      number: this.resourceMainStore.getDocumentNumberTextResource(),
-      date: this.resourceMainStore.getDocumentDateTextResource(),
-      fieldLabel: this.resourceMainStore.getDocumentDescriptionFieldLabelTextResource()
-    };
-
-  }
-
-  formGenerator() {
-
-    this.documentForm = new FormGroup({
-      documentNumber: new FormControl('0000000000'),
-      documentDate: new FormControl('1401-02-05'),
-      description: new FormControl('')
-    });
-
-    this.formOutputHandler.emit(this.documentForm);
-
-  }
 
 }
