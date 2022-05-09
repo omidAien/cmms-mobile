325d4486a8792a2dbf8e3b5c82c89d1daf69a9da
Omid Aiene <omid.aienad@gmail.com>
2022-05-09 09:47:35 +0430

    update #2 adding open()-method to BottomSheetOperationsService 14010219

--- src/app/shared/services/bottom-sheet-operations.service.ts
+++ src/app/shared/services/bottom-sheet-operations.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { OperationButton } from '../appModels';
+import { BottomSheetOperationsHandlerComponent } from '../reusableComponents/bottom-sheet-operations-handler/bottom-sheet-operations-handler.component';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +12,25 @@ export class BottomSheetOperationsService {
   private buttonsSubject = new BehaviorSubject<OperationButton[]>(null);
   buttons$: Observable<OperationButton[]> = this.buttonsSubject.asObservable();
 
-  constructor() { }
+  constructor(private bottomSheet: MatBottomSheet) { }
 
   set(value: OperationButton[]) {
 
     this.reset();
 
     this.buttonsSubject.next(value);
+
+  }
+
+  private hasValue(): boolean {
+
+    return this.buttonsSubject.getValue() ? true : false;
+
+  }
+
+  open(): void {
+
+    this.hasValue() ? this.bottomSheet.open(BottomSheetOperationsHandlerComponent) : null;
 
   }
 
